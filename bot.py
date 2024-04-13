db50095d4cbd2d05f3469b27bede3eb1c953f8f3
KodlandLVL3 <155145450+KodlandLVL3@users.noreply.github.com>
2024-04-13 16:54:14 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,57 @@
+from telebot import TeleBot
+import telebot
+from logic import TaskManager
+bot = TeleBot("6077941896:AAGbReizYk_FxNsV40lrmF1n7lBsW548cz8")
+
+task_manager = TaskManager("database.db")
+task_manager.create_table()
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, """Привет! Я бот-менеджер задач 
+Помогу тебе сохранить твои задачи!) 
+
+/add_task - используй для добавления новой задачи
+/delete_task - используй для удаления задачи                                      
+                     """)
+    
+
+@bot.message_handler(commands=['add_task'])
+def addtask_command(message):
+    bot.send_message(message.chat.id, "Введите название задачи:")
+    bot.register_next_step_handler(message, save_task)
+
+def save_task(message):
+    name = message.text
+    user_id = message.from_user.id 
+    task_manager.add_task(user_id, name, '')
+    bot.send_message(message.chat.id, "Задача добавлена")
+
+@bot.message_handler(commands=['delete_task'])
+def deletetask_command(message):
+    bot.send_message(message.from_user.id, "Введите имя задачи, которую хотите удалить:")
+    bot.register_next_step_handler(message, delete_task_by_id)
+
+def delete_task_by_id(message):
+    user_id = message.from_user.id  
+    task_name = message.text
+    task_manager.delete_task(task_name, user_id)
+    bot.send_message(message.chat.id, "Задача удалена")
+
+@bot.message_handler(commands=['show']) 
+def show(message):
+    user_id = message.from_user.id 
+    arg = telebot.util.extract_arguments(message.text)
+    try:
+        arg = int(arg)
+        tasks = task_manager.show_task_many(user_id, arg)
+    except:
+        tasks = task_manager.show_task_all(user_id)
+    if tasks:
+        tasks =  "\n".join([x[0] for x in tasks])
+        bot.send_message(message.chat.id, tasks)
+    else:
+        bot.send_message(message.chat.id, "Задач нет")
+        
+
+bot.infinity_polling()
---
+++ logic.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+class TaskManager:
+    def __init__(self, database):
+        self.database = database
+
+    def create_table(self): 
+        conn = sqlite3.connect(self.database)
+        with conn:
+            conn.execute("""
+            CREATE TABLE IF NOT EXISTS tasks(
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            description TEXT,
+            user_id INTEGER NOT NULL
+            )""")
+            conn.commit()
+
+    def add_task(self, user_id, name, description): 
+        conn = sqlite3.connect(self.database)
+        with conn:
+            conn.execute("INSERT INTO tasks (name, description, user_id) VALUES (?, ?, ?)", (name, description, user_id))
+            conn.commit()
+
+    def delete_task(self, task_name, user_id): 
+        conn = sqlite3.connect(self.database)
+        with conn:
+            conn.execute("DELETE FROM tasks WHERE name = ? AND user_id = ?", (task_name, user_id))
+            conn.commit()
+
+    def show_task(self, user_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT name FROM tasks WHERE user_id = ?", (user_id,))
+            return cur
+            
+    def show_task_all(self, user_id):
+        cur = self.show_task(user_id)
+        return cur.fetchall()
+
+    def show_task_many(self, user_id, num):
+        cur = self.show_task(user_id)
+        return cur.fetchmany(num)
